1441fa43e3b6e631946b9e4073ba2b2fe245e041
Aleksandr Khmelinin <aakhmelinin@gmail.com>
2020-06-16 19:41:37 +0300

    fix

--- src/main/java/reporting/MyLogger.java
+++ src/main/java/reporting/MyLogger.java
@@ -11,10 +11,6 @@ public class MyLogger {
     logger.info(message);
   }
 
-  public static void error(String message) {
-    logger.error(message);
-  }
-
   public static void warn(String message) {
     logger.warn(message);
   }
